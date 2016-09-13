227fc535eccb8f448d03cf35bcf722dd72c4b17d
rg83 <rgutha83@gmail.com>
2016-09-13 13:21:57 -0400

    Initial commit

---
+++ src/main/java/org/tr/fileprocessing/FileProcessor.java
@@ -0,0 +1,38 @@
+package org.tr.fileprocessing;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.StringWriter;
+
+public class FileProcessor {
+
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			throw new IllegalArgumentException("Invalid input args !!!!");
+		}
+
+		File file = new File(args[0]);
+		System.out.println(file);
+		if (!file.exists()) {
+			throw new RuntimeException("Input file doesn't exists !!!");
+		}
+
+		printFileData(file);
+	}
+
+	public static void printFileData(File file) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			StringWriter writer = new StringWriter();
+
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				writer.write(line);
+				writer.write("\n");
+			}
+			System.out.println(writer.toString());
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+}
